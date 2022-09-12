e4d2a4c37f677efdaf2a352c1e0b251909c8d24b
Karla-Ruano <113318238+Karla-Ruano@users.noreply.github.com>
2022-09-12 00:43:50 -0600

    Add files via upload

---
+++ Ejercicio 5 Periodo 1 Sem 4 Clase 12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float final, contador;
+	cout << "Ingrese el limite que desea: ";
+	cin >> final;
+		while (contador < final){
+		contador = contador + 1;
+		cout << "Contador: " <<contador << endl;	
+		}
+}
